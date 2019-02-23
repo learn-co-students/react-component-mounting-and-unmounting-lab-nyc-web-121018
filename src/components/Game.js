84a18e23d18fc662ed3a23c3103e52ad2e70a8c8
Nison Malayev <nmalayev@gmail.com>
2019-02-23 18:10:25 -0500

    Done.

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Pancake from './Pancake';
 
 class Game extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -17,16 +16,20 @@ class Game extends React.Component {
   }
 
   // TODO: create a componentDidMount() which will set the current time
-  
-  setCurrentTime = () => {
-    this.setState({ time: new Date(Date.now())});
+
+  componentDidMount() {
+    this.setCurrentTime();
   }
+
+  setCurrentTime = () => {
+    this.setState({ time: new Date(Date.now()) });
+  };
 
   addPancake = () => {
     this.setState({
       pancakes: this.state.pancakes.concat(Date.now())
     });
-  }
+  };
 
   takeItOff = (id, status) => {
     const { pancakes, cooked, burnt, raw } = this.state;
@@ -37,29 +40,29 @@ class Game extends React.Component {
       burnt: status === 'burnt' ? burnt + 1 : burnt,
       raw: status === 'raw' ? raw + 1 : raw
     });
-  }
+  };
 
   render() {
+    // console.log(this.state.pancakes);
     const { pancakes, burnt, cooked, raw, time } = this.state;
-    const pans = pancakes.map((pancake, index) => <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />);
+    const pans = pancakes.map((pancake, index) => (
+      <Pancake key={index} id={pancake} takeItOff={this.takeItOff} />
+    ));
 
     return (
-      <div className="Game">
+      <div className='Game'>
         <span>Pancake shop opened at: {time ? time.toString() : ''}</span>
         <div>
-          <div className="Game__score --cooked">Cooked: {cooked}</div>
-          <div className="Game__score --burnt">Burnt: {burnt}</div>
-          <div className="Game__score --raw">Raw: {raw}</div>
+          <div className='Game__score --cooked'>Cooked: {cooked}</div>
+          <div className='Game__score --burnt'>Burnt: {burnt}</div>
+          <div className='Game__score --raw'>Raw: {raw}</div>
         </div>
-        <button
-          onClick={this.addPancake}
-          className="Game__button"
-        >
+        <button onClick={this.addPancake} className='Game__button'>
           New pancake!
         </button>
-        <div className="Game__pancakes">{pans}</div>
+        <div className='Game__pancakes'>{pans}</div>
       </div>
-    )
+    );
   }
 }
 
